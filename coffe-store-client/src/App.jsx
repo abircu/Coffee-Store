3329141f83710592b6d52683b1f118a0bbf6672a
abircu <mdabircse4873@gmail.com>
2023-10-14 21:51:40 +0530

    create a coffe-store backend project with  mongoDB database

---
+++ coffe-store-client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-purple-600">Best coffe</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ coffe-store-client/src/Components/AddCoffe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCoffe = () => {
+  return (
+    <div>
+      <h4>Add coffe</h4>
+    </div>
+  );
+};
+
+export default AddCoffe;
